b0c82b99e94d592630ded96246cca1cfb54c09ea
vyau987 <vyau@hotmail.co.uk>
2014-02-27 16:13:41 +0000

    Updated files
    Si to add functionality

--- Database.java
+++ Database.java
@@ -47,7 +47,7 @@ public class Database {
 	}
 
 	// http://stackoverflow.com/questions/453018/number-of-lines-in-a-file-in-java
-	public int countFileLines(String filename) throws IOException {
+	private int countFileLines(String filename) throws IOException {
 		InputStream is = new BufferedInputStream(new FileInputStream(filename));
 		try {
 			byte[] c = new byte[1024];
@@ -75,56 +75,90 @@ public class Database {
 		}
 	}
 
-	public String[][] getAllStaff()
+	public String[] getAllStaff()
 	{
+		
+		String[] allStaff = null;
+		int lineCount = 0;
+		
+		
+		while(fileReader.hasNext())
+		{
+
+			lineCount++;
+			fileReader.nextLine();
+
+		}
+		
+		
+		allStaff = new String[lineCount];
+		
+		resetFileReader();
+		
+		int count = 0;
+		while(fileReader.hasNext())
+		{
+			allStaff[count] = fileReader.nextLine();
+			count++;
+		}
+		
+		return allStaff;
+	}
+
+	public int countForms(String moduleCode){
 		try
 		{
-			int totalLineCount = countFileLines("staff.txt");
-			String[][] allStaff = new String[totalLineCount][6];
+			System.out.println("starts getAllForms method");
+			int totalLineCount = countFileLines("forms.txt");
+			int numForms = 0;
 			for( int i=0; i<totalLineCount; i++ )
 			{
 				String line = fileReader.nextLine();
-				String[] parts = line.split(",");
-				parts[5] = parts[5].substring(0, parts[5].length()-1);
-				allStaff[i][0] = parts[0];
-				allStaff[i][1] = parts[1];
-				allStaff[i][2] = parts[2];
-				allStaff[i][3] = parts[3];
-				allStaff[i][4] = parts[4];
-				allStaff[i][5] = parts[5];
+				if ( line.contains(moduleCode) == true ){
+					numForms++;
+				}
 			}
-			return allStaff;
+			fileReader.close();
+			System.out.println("not crashed yet...");
+			return numForms;
 		}
 		catch (IOException e)
 		{
 			e.printStackTrace();
-			return null;
+			return -1;
 		}
 	}
-
-	public String[][] getAllForms(String filename)
+	
+	public String[][] getAllForms(String moduleCode)
 	{
 		try
 		{
-			int totalLineCount = countFileLines("forms.txt");
-			String[][] allForms = new String[totalLineCount][6];
-			for( int i=0; i<totalLineCount; i++ )
+			int numForms = countForms(moduleCode);
+			System.out.println( numForms );
+			String[][] moduleForms = new String[numForms][7];
+			for( int i=0; i<countFileLines("forms.txt"); i++ )
 			{
 				String line = fileReader.nextLine();
-				
-				//[0] moduleCode; [1] paperName; [2] ELODeadline; [3] EMDeadline; [4] MLDeadline; [5] IMDeadline; [6] EM_ID; [7] IM_ID
-				String[] parts = line.split(",");
-				parts[7] = parts[7].substring(0, parts[7].length()-1);
-				allForms[i][0] = parts[0];
-				allForms[i][1] = parts[1];
-				allForms[i][2] = parts[2];
-				allForms[i][3] = parts[3];
-				allForms[i][4] = parts[4];
-				allForms[i][5] = parts[5];
-				allForms[i][6] = parts[6];
-				allForms[i][7] = parts[7];
+				System.out.println("nothing");
+				if ( line.contains(moduleCode)){
+					String[] parts = line.split(",");
+					parts[7] = parts[7].substring(0, parts[7].length()-1);
+					System.out.println( parts[7]);
+					//[0] moduleCode; [1] paperName; [2] ELODeadline; [3] EMDeadline; 
+					//[4] MLDeadline; [5] IMDeadline; [6] EM_ID; [7] IM_ID
+					moduleForms[i][0] = parts[0];
+					moduleForms[i][1] = parts[1];
+					moduleForms[i][2] = parts[2];
+					moduleForms[i][3] = parts[3];
+					moduleForms[i][4] = parts[4];
+					moduleForms[i][5] = parts[5];
+					moduleForms[i][6] = parts[6];
+					moduleForms[i][7] = parts[7];
+				}
 			}
-			return allForms;
+			fileReader.close();
+			System.out.println("forms table saved");
+			return moduleForms;
 		}
 		catch (IOException e)
 		{
@@ -1003,7 +1037,12 @@ public class Database {
 	public static void main(String[] args)
 	{
 		/*Database d = new Database();
-		d.initDatabase("staff.txt");
+		d.initDatabase("forms.txt");
+		d.insertAssessmentPaper("cm2304", "cw1", "ELODate1", "EMDeadline1", "MLDeadline1", "IMDeadline1", "1001001", "2002002");
+		d.insertAssessmentPaper("cm2304", "cw2", "ELODate2", "EMDeadline2", "MLDeadline2", "IMDeadline2", "1001001", "2002002");
+		d.insertAssessmentPaper("cm2304", "cw3", "ELODate3", "EMDeadline3", "MLDeadline3", "IMDeadline3", "1001001", "2002002");
+		d.insertAssessmentPaper("cm2304", "cw4", "ELODate4", "EMDeadline4", "MLDeadline4", "IMDeadline4", "1001001", "2002002");
+		
 		d.insertStaff("Simon", "Titcomb", "LOLNOPE@nah.com", "2345678986", "nahmate", "TitS");
 		d.insertStaff("Aly", "Sheriff", "kingtut@pharoahpower.com", "11111", "egyptrules", "Tut");
 		d.insertStaff("Mez", "Gangbanger", "kingofrape@ianwatkins.com", "5678987", "kids", "Kids101");
@@ -1015,10 +1054,24 @@ public class Database {
 		String mez = d.getStaffEmail("first name", "mez");
 		System.out.println(mez);
 		System.out.println(d.deleteStaff("staffID","Tut"));
-
+		
 		d.close();
 
 		System.out.println("Completed.");*/
+		
+		Database d = new Database();
+		d.initDatabase("forms.txt");
+
+		System.out.println("Started.");
+		
+		String[] a = d.getAllStaff();
+		for(int i=0; i < a.length; i++)
+		{
+			System.out.println(a[i]);
+		}
+
+		System.out.println("Completed.");
+	
 
 	}
 
