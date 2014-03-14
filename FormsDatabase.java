7aba13aebe5d3587cfac00a068d480bf5b3c8cf4
vyau987 <vyau@hotmail.co.uk>
2014-03-14 17:42:49 +0000

    Edited database classes
    Pushed methods from Database.java to FormsDatabase.java, edited
    getAllForms method (now getModuleForms), new methods added too

--- FormsDatabase.java
+++ FormsDatabase.java
@@ -1,15 +1,129 @@
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 
 
 public class FormsDatabase extends Database
 {
 
+	// http://stackoverflow.com/questions/453018/number-of-lines-in-a-file-in-java
+	private int countFileLines(String filename) throws IOException {
+		InputStream is = new BufferedInputStream(new FileInputStream(filename));
+		try {
+			byte[] c = new byte[1024];
+			int count = 0;
+			int readChars = 0;
+			boolean empty = true;
+			while ((readChars = is.read(c)) != -1) {
+				empty = false;
+				for (int i = 0; i < readChars; ++i) {
+					if (c[i] == '\n') {
+						++count;
+					}
+				}
+			}
+			return (count == 0 && !empty) ? 1 : count;
+		} 
+		catch (IOException e)
+		{
+			e.printStackTrace();
+			return -1;
+		}
+		finally 
+		{
+			is.close();
+		}
+	}
+
+
+
+	public int countForms(String moduleCode){
+		try
+		{
+			System.out.println("starts getAllForms method");
+			int totalLineCount = countFileLines("forms.txt");
+			int numForms = 0;
+			for( int i=0; i<totalLineCount; i++ )
+			{
+				String line = fileReader.nextLine();
+				if ( line.contains(moduleCode) == true ){
+					numForms++;
+				}
+			}
+			fileReader.close();
+			System.out.println("not crashed yet...");
+			
+			resetFileReader();
+			
+			return numForms;
+			
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+			return -1;
+		}
+	}
+	
+	/*
+	 * forms:
+	 * [0] moduleCode; [1] paperName; [2] ELODeadline; [3] EMDeadline;
+	 * [4] MLDeadline; [5] IMDeadline; [6] EM_ID; [7] IM_ID;
+	 * [8] ELOCompleted; [9] EMCompleted; [10] MLCompleted; [11] IMCompleted
+	 */
+	public String[][] getModuleForms(String moduleCode)
+	{
+		try
+		{
+			int numForms = countForms(moduleCode);
+			System.out.println( numForms );
+			String[][] moduleForms = new String[numForms][12];
+			for( int i=0; i<countFileLines("forms.txt"); i++ )
+			{
+				String line = fileReader.nextLine();
+				//System.out.println("nothing");
+				if ( line.contains(moduleCode)){
+					String[] parts = line.split(",");
+					
+					moduleForms[i][0] = parts[0];
+					moduleForms[i][1] = parts[1];
+					moduleForms[i][2] = parts[2];
+					moduleForms[i][3] = parts[3];
+					moduleForms[i][4] = parts[4];
+					moduleForms[i][5] = parts[5];
+					moduleForms[i][6] = parts[6];
+					moduleForms[i][7] = parts[7];
+					moduleForms[i][8] = parts[8];
+					moduleForms[i][9] = parts[9];
+					moduleForms[i][10] = parts[10];
+					moduleForms[i][11] = parts[11].substring(0, parts[11].length()-1);
+					
+					//System.out.println("moduleForms[i][11]"+moduleForms[i][11]);
+				}
+			}
+			fileReader.close();
+			System.out.println("forms table saved");
+			resetFileReader();
+			
+			return moduleForms;
+			
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+			return null;
+		}
+	}
 
 	public boolean insertAssessmentPaper(String moduleCode, String paperName, String ELODeadline,
-			String EMDeadline, String MLDeadline, String IMDeadline, String EM_ID, String IM_ID)
+			String EMDeadline, String MLDeadline, String IMDeadline, String EM_ID, String IM_ID,
+			String ELOCompleted, String EMCompleted, String MLCompleted, String IMCompleted)
 	{
 		fileReader.close();
 
@@ -22,13 +136,42 @@ public class FormsDatabase extends Database
 		{
 			e.printStackTrace();
 		}
-		fileWriter.println(moduleCode+","+paperName+","+ELODeadline+","+EMDeadline+","+MLDeadline+","+IMDeadline+","+EM_ID+","+IM_ID+";");
+		fileWriter.println(moduleCode+","+paperName+","+ELODeadline+","+EMDeadline+
+				","+MLDeadline+","+IMDeadline+","+EM_ID+","+IM_ID+
+				","+ELOCompleted+","+EMCompleted+","+MLCompleted+","+IMCompleted+";");
 		fileWriter.close();
 
 		resetFileReader();
 		return true;
 	}
 
+	public boolean deleteAssessmentPaper(String moduleCode, String paperName)
+	{
+		//int number_lines = countFileLines("forms.txt");
+		while( fileReader.hasNext()){
+			
+			String line = fileReader.nextLine();
+			if ( line.contains(moduleCode) && line.contains(paperName) ){
+				//delete row from forms file
+				
+				
+				//archive the row to another file and add another column for current date 
+				// Date d = new Date
+				
+				// insert new row - using insertAssessmentPaper method above
+				
+			}
+		}
+		
+		return true;
+		
+	}
+	
+	public boolean archivePaper(String entry){
+		// TODO fill here
+		return false;
+	}
+	
 	public String[] getAssessmentPapers(String searchAttribute, String searchValue)
 	{
 		String[] module = new String[8];
